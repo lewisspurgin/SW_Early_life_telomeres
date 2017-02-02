7713f4c362c9f35f85f15fb67cda34c4e6db4d94
lgs85 <lewisspurgin@gmail.com>
2017-02-02 17:41:35 +0000

    Edits

--- Scripts/SW juvenile telomeres IPC models.r
+++ Scripts/SW juvenile telomeres IPC models.r
@@ -6,16 +6,16 @@
 
 # Telomere length, age and cohort -----------------------------------------
 
-RTL_Age <- lmer(RTL ~ Cohort*(Agemonths + LogAge + FAge + Age2) + (1|BirdID) + (1|CatchYear) + (1|PlateID),
+RTL_Age <- lmer(RTL ~ Cohort*(Agemonths + LogAge + FAge + Age2) + (1|BirdID) + (1|CatchYear),
                 data=dd,
                 REML = FALSE)
-RTL_LogAge <- lmer(RTL ~ LogAge + (1|BirdID) + (1|CatchYear) + (1|PlateID),
+RTL_LogAge <- lmer(RTL ~ LogAge + (1|BirdID) + (1|CatchYear),
                 data=dd,
                 REML = FALSE)
-RTL_DeltaAge <- lmer(RTL ~ MeanAge + Cohort*(DeltaAge + DeltaLogAge + DeltaAge2) + (1|BirdID) + (1|CatchYear) + (1|PlateID),
+RTL_DeltaAge <- lmer(RTL ~ MeanAge + Cohort*(DeltaAge + DeltaLogAge + DeltaAge2) + (1|BirdID) + (1|CatchYear),
                 data=dd3,
                 REML = FALSE)
-RTL_DeltaLogAge <- lmer(RTL ~ DeltaLogAge + (1|BirdID) + (1|CatchYear) + (1|PlateID),
+RTL_DeltaLogAge <- lmer(RTL ~ DeltaLogAge + (1|BirdID) + (1|CatchYear),
                    data=dd3,
                    REML = FALSE)
 RTL_RTL1 <- lmer(RTL1 ~ RTL + LogAge + (1|BirdID) + (1|LayYear),
@@ -27,13 +27,13 @@ RTL_RTL1 <- lmer(RTL1 ~ RTL + LogAge + (1|BirdID) + (1|LayYear),
 ddNA <- subset(subset(dd,!is.na(Insect)),!is.na(TQ))
 dd3NA <- subset(subset(dd3,!is.na(Insect)),!is.na(TQ))
 
-RTL_Full <- lmer(RTL ~  LogAge + Tarsus + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID) + (1|PlateID),
+RTL_Full <- lmer(RTL ~  LogAge + Tarsus + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID),
                  data = ddNA,
                  REML = FALSE)
-RTL_Full_NoTarsus <- lmer(RTL ~  LogAge + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID) + (1|PlateID),
+RTL_Full_NoTarsus <- lmer(RTL ~  LogAge + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID),
                           data = ddNA,
                           REML = FALSE)
-RTL_Full_SexTarsus <- lmer(RTL ~  LogAge + Helper + GroupSize + Sex + BodyMass + Insect + Density + Sex*Tarsus + TQ + (Agemonths|LayYear) + (1|BirdID) + (1|PlateID),
+RTL_Full_SexTarsus <- lmer(RTL ~  LogAge + Helper + GroupSize + Sex + BodyMass + Insect + Density + Sex*Tarsus + TQ + (Agemonths|LayYear) + (1|BirdID),
                           data = ddNA,
                           REML = FALSE)
 DeltaRTL_Full <- lmer(DeltaRTL ~  LogAge + Tarsus + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (1|BirdID),
