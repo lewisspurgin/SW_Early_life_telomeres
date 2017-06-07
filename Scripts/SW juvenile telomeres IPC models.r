3b482b442e4cd7c105f824950934cf5be4490a33
lgs85 <lewisspurgin@gmail.com>
2017-06-07 17:32:05 +0100

    Edits

--- Scripts/SW juvenile telomeres IPC models.r
+++ Scripts/SW juvenile telomeres IPC models.r
@@ -13,16 +13,16 @@ RTL_ST <- lmer(RTL ~ StorageTime + (1|BirdID) + (1|PlateID),
 RTL_Age <- lmer(RTL ~ Cohort + Agemonths + LogAge + FAge + Age2 + (1|BirdID) + (1|CatchYear) + (1|PlateID),
                 data=dd,
                 REML = FALSE)
-RTL_LogAge <- lmer(RTL ~ LogAge + Cohort + (1|BirdID) + (1|CatchYear),
+RTL_LogAge <- lmer(RTL ~ LogAge + Cohort + (1|BirdID) + (1|CatchYear)+ (1|PlateID),
                 data=dd,
                 REML = FALSE)
-RTL_LogAgeXcohort <- lmer(RTL ~ LogAge*Cohort + (1|BirdID) + (1|CatchYear),
+RTL_LogAgeXcohort <- lmer(RTL ~ LogAge*Cohort + (1|BirdID) + (1|CatchYear)+ (1|PlateID),
                    data=dd,
                    REML = FALSE)
-RTL_DeltaAge <- lmer(RTL ~ MeanAge + Cohort + DeltaAge + DeltaLogAge + DeltaAge2 + (1|BirdID) + (1|CatchYear),
+RTL_DeltaAge <- lmer(RTL ~ MeanAge + Cohort + DeltaAge + DeltaLogAge + DeltaAge2 + (1|BirdID) + (1|CatchYear)+ (1|PlateID),
                 data=dd3,
                 REML = FALSE)
-RTL_DeltaLogAge <- lmer(RTL ~ DeltaLogAge + (1|BirdID) + (1|CatchYear),
+RTL_DeltaLogAge <- lmer(RTL ~ DeltaLogAge + (1|BirdID) + (1|CatchYear)+ (1|PlateID),
                    data=dd3,
                    REML = FALSE)
 DeltaRTL_LogAge <- lmer(DeltaRTL ~ LogAge + (1|BirdID) + (1|CatchYear),
@@ -40,7 +40,7 @@ RTL_RTL1 <- lmer(RTL1 ~ RTL + LogAge + (1|BirdID) + (1|LayYear),
 ddNA <- subset(subset(dd,!is.na(Insect)),!is.na(TQ))
 dd3NA <- subset(subset(dd3,!is.na(Insect)),!is.na(TQ))
 
-RTL_Full <- lmer(RTL ~  LogAge + Tarsus + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID),
+RTL_Full <- lmer(RTL ~  LogAge + Tarsus + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID)+ (1|PlateID),
                  data = ddNA,
                  REML = FALSE)
 RTL_Full_NoTarsus <- lmer(RTL ~  LogAge + Helper + GroupSize + Sex + BodyMass + Insect + Density + TQ + (Agemonths|LayYear) + (1|BirdID),
